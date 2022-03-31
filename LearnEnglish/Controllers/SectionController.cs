de1d8854ce90c13de91482fd21b6439cc4a89423
celil öztürk <celil.ozturk@windowslive.com>
2022-04-01 00:41:42 +0200

    Section Area -> Edit, Add and Index page edited.

--- LearnEnglish/Controllers/SectionController.cs
+++ LearnEnglish/Controllers/SectionController.cs
@@ -29,9 +29,9 @@ namespace LearnEnglish.Controllers
 
         public IActionResult Add()
         {
-           // var themeList = _db.Themes.ToList();
+            // var themeList = _db.Themes.ToList();
             //ViewBag.themeList = themeList;
-            ViewBag.Themes = new SelectList(_db.Themes,"ThemeId","Title");
+            ViewBag.Themes = new SelectList(_db.Themes, "ThemeId", "Title");
             return View();
         }
         [HttpPost]
@@ -44,7 +44,7 @@ namespace LearnEnglish.Controllers
                 var section = new Section();
                 section.Title = Section.Title;
                 section.ThemeId = Section.ThemeId;
-                section.CreatedDate=DateTime.Now;
+                section.CreatedDate = DateTime.Now;
                 section.Rank = byte.MinValue;
                 _db.Sections.Add(section);
                 _db.SaveChanges();
@@ -58,31 +58,29 @@ namespace LearnEnglish.Controllers
                 ajaxResponse.Message = "something went wrong !";
                 ajaxResponse.DetailMessage = e.InnerException.Message;
             }
-
-            //return Content(ajaxResponse.Result.ToString());
-             return Json(ajaxResponse);
+            return Json(ajaxResponse);
 
         }
 
         public IActionResult GetSection()
         {
             var ajaxResponse = new AjaxResponse();
-            //var sections = _db.Sections.Include("Theme").ToList();
             var sections = from s in _db.Sections
-                join t in _db.Themes
-                    on s.ThemeId equals t.ThemeId
-                select new
-                {
-                    Title=s.Title,
-                    CreatedDate=s.CreatedDate
-                    
-                };
+                           join t in _db.Themes 
+                               on s.ThemeId equals t.ThemeId
+                           select new
+                           {   ThemeTitle=t.Title,
+                               SectionId=s.SectionId,
+                               Title = s.Title,
+                               Rank=s.Rank,
+                               CreatedDate = s.CreatedDate
+                           };
 
             try
             {
                 ajaxResponse.Data = sections;
                 ajaxResponse.Result = true;
-                ajaxResponse.Message = "Everything is ok !";
+                ajaxResponse.Message = "Section added successfully !";
 
             }
             catch (Exception e)
@@ -91,6 +89,24 @@ namespace LearnEnglish.Controllers
                 ajaxResponse.Message = "Something went wrong !!";
             }
             return Json(ajaxResponse);
+        }
+
+        [Route("Section/Details/{id}")]
+        public IActionResult Details(string id)
+        {
+            var ajaxResponse = new AjaxResponse();
+            var section = _db.Sections.Where(s => s.SectionId == (int.Parse(id))).SingleOrDefault();
+            SectionViewModel sectionViewModel = new SectionViewModel{ ThemeId = section.ThemeId, Title = section.Title};
+           
+            ViewBag.Themes = new SelectList(_db.Themes, "ThemeId", "Title");
+            return View("Edit",sectionViewModel);
+           // return Content($"id:{section.ThemeId} {section.Title} {section.SectionId}");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Section model)
+        {
+            return View();
         }
     }
 }
--- LearnEnglish/wwwroot/js/pages/section.js
+++ LearnEnglish/wwwroot/js/pages/section.js
@@ -17,6 +17,39 @@
             }
         },
         errorElement: 'span',
+        errorPlacement: function(error, element) {
+            error.addClass('invalid-feedback');
+            element.closest('.form-group').append(error);
+        },
+        highlight: function(element, errorClass, validClass) {
+            $(element).addClass('is-invalid');
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).removeClass('is-invalid');
+        },
+
+        submitHandler: function(form) {
+            SectionAdd();
+        }
+    });
+    $("#form_section_edit").validate({
+        rules: {
+            Title: {
+                required: true
+            },
+            ThemeId: {
+                required: true
+            }
+        },
+        messages: {
+            Title: {
+                required: "Section title is required..."
+            },
+            ThemeId: {
+                required: "Please select a Theme..."
+            }
+        },
+        errorElement: 'span',
         errorPlacement: function (error, element) {
             error.addClass('invalid-feedback');
             element.closest('.form-group').append(error);
@@ -29,11 +62,10 @@
         },
 
         submitHandler: function (form) {
-            SectionAdd();
+            SectionEdit();
         }
     });
-
-})
+});
 function SectionAdd() {
     BlockPage();
     var formData = $('#form_section_add').serialize();
@@ -76,18 +108,97 @@ function SectionGet() {
         dataType: 'json',
         success: function (response) {
             if (response.result) {
-                alert(response.result);
+                var sectionRowCount = response.data.length;
+                if (sectionRowCount == 0) {
+                    $('#section_list tbody').append('No record has been added yet....');
+                }
+                else {
+                    var sections = '';
+                    $.each(response.data, function (index, row) {
+                        var shortDate = ShortDate(row.createdDate);
+
+                        sections += '<tr><td class="text-success font-weight-bold">'+row.themeTitle
+                            + '</td><td>' + row.sectionId
+                            + '</td><td>' + row.title
+                            + '</td><td>' + row.rank
+                            + '</td><td>' + shortDate
+                            + '</td><td>' + '<a class="btn btn-success" href="/Section/Details/' + row.sectionId + '">Edit</a>  '
+                            + '<button type = "button" class="btn btn-danger" onclick = "SectionDeleteConfirm(' + row.sectionId + ')" ><i class="fa fa-sm fa-trash-alt"></i> Delete</button >'
+                            + '</td></tr>'
+                    });
+                    $('#section_list tbody').append(sections);
+                }
                
             }
-            alert(response.result);
+           
 
         },
         error: function (request, status, error) {
         },
         complete: function () {
-            
             return false;
         }
     });
 
+}
+function ShortDate(longDate) {
+    var createdDate = new Date(longDate);
+    var d = createdDate.getDate();
+    var m = createdDate.getMonth();
+    m += 1;  // JavaScript months are 0-11
+    var y = createdDate.getFullYear();
+    var shortDate = (d + "." + m + "." + y);
+    return shortDate;
+}
+function ThemeDeleteConfirm(sectionId) {
+    var message = sectionId;
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete Section !'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            ThemeDelete(sectionId);
+        }
+    });
+}
+function ThemeDelete(sectionId) {
+    var data = { "SectionId": sectionId }
+    $.ajax({
+        type: 'POST',
+        url: '/Section/Delete',
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        data: data,
+        dataType: 'json',
+        success: function (response) {
+            if (response.result) {
+                Swal.fire(
+                    'Deleted!',
+                    response.message,
+                    'success'
+                );
+                //ThemeGet();
+            } else {
+                Swal.fire(
+                    'Uyarı!',
+                    response.message,
+                    'error'
+                );
+            }
+        },
+        error: function (request, status, error) {
+            alert(request + status + error);
+        },
+        complete: function () {
+            UnBlockPage();
+            return false;
+        }
+    });
+}
+function SectionEdit() {
+    alert("Section Edit");
 }
